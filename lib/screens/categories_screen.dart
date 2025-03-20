e07d56b38f78299a8c38f03522a5eaf89bd97ba3
walidmadad <walid.madad123@gmail.com>
2025-03-21 00:16:43 +0100

    update categories_screen.dart

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Travel App"),
+        backgroundColor: Colors.blue[700],
+      ),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 7 / 8,
+          mainAxisExtent: 10,
+          crossAxisSpacing: 10
+        ),
+        children: [],
+      ),
+    );
+  }
+}
