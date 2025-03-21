28db6937ee443b58fb983d14c75aecd18e6bbcbd
walidmadad <walid.madad123@gmail.com>
2025-03-21 23:48:14 +0100

    add category_item.dart

---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem({super.key, required this.title, required this.imageUrl});
+
+  final String title;
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: Image.network(imageUrl, height: 200, fit: BoxFit.cover),
+        ),
+        Container(
+          padding: EdgeInsets.all(10),
+          alignment: Alignment.center,
+
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.black.withOpacity(0.5),
+          ),
+          child: Text(
+            title,
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ],
+    );
+  }
+}
