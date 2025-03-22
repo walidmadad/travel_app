48ad4b8d3d90c1cc7d57065122800988bb24845a
walidmadad <walid.madad123@gmail.com>
2025-03-22 20:24:36 +0100

    update categories_items.dart

--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,33 +1,53 @@
 import 'package:flutter/material.dart';
+import '../screens/categories_trips_screen.dart';
 
 class CategoryItem extends StatelessWidget {
-  const CategoryItem({super.key, required this.title, required this.imageUrl});
-
+  const CategoryItem({
+    super.key,
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+  });
+  final String id;
   final String title;
   final String imageUrl;
 
+  void selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder:
+            (c) => CategoriesTripsScreen(categoryId: id, categoryTitle: title),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(15),
-          child: Image.network(imageUrl, height: 250, fit: BoxFit.cover),
-        ),
-        Container(
-          padding: EdgeInsets.all(10),
-          alignment: Alignment.center,
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Image.network(imageUrl, height: 250, fit: BoxFit.cover),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            alignment: Alignment.center,
 
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            color: Colors.black.withOpacity(0.5),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.black.withOpacity(0.5),
+            ),
+            child: Text(
+              title,
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
           ),
-          child: Text(
-            title,
-            style: TextStyle(color: Colors.white, fontSize: 20),
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
