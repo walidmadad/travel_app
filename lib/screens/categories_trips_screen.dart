03ccaae719cf40a191a06f762bd664afdc45565d
walidmadad <walid.madad123@gmail.com>
2025-03-22 23:55:38 +0100

    update categories_trips_screen.dart

--- lib/screens/categories_trips_screen.dart
+++ lib/screens/categories_trips_screen.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
+import '../data.dart';
 
 class CategoriesTripsScreen extends StatelessWidget {
-  final String categoryId;
-  final String categoryTitle;
-  const CategoriesTripsScreen({
-    super.key,
-    required this.categoryId,
-    required this.categoryTitle,
-  });
+  static const screenRoute = "category-trips";
   @override
   Widget build(BuildContext context) {
+    final routeAgrument =
+        ModalRoute.of(context)?.settings.arguments as Map<String, String>;
+    final String? categoryId = routeAgrument['id'];
+    final String? categoryTitle = routeAgrument['title'];
+    final filtredTrips =
+        tripsData.where((trip) {
+          return trip.categories.contains(categoryId) ? true : false;
+        }).toList();
     return Scaffold(
-      appBar: AppBar(title: Text(categoryTitle)),
-      body: Center(child: Text("data")),
+      appBar: AppBar(title: Text(categoryTitle!)),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) => Text(filtredTrips[index].title),
+        itemCount: filtredTrips.length,
+      ),
     );
   }
 }
