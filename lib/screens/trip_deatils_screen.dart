5dfd835831ef17c2bd844b55f8ccebf727f74d89
walidmadad <walid.madad123@gmail.com>
2025-03-23 01:24:15 +0100

    add trip_details_screen

---
+++ lib/screens/trip_deatils_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../models/trip.dart';
+
+class TripDeatilsScreen extends StatelessWidget {
+  const TripDeatilsScreen({super.key});
+  static String routeName = "/trip-details";
+
+  @override
+  Widget build(BuildContext context) {
+    final Trip trip = ModalRoute.of(context)?.settings.arguments as Trip;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(trip.title, style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blue[700],
+      ),
+      body: Center(),
+    );
+  }
+}
