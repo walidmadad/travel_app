2e340cb18afba2f28994100cad54f08c3284cfa2
walidmadad <walid.madad123@gmail.com>
2025-03-23 16:02:26 +0100

    update categoris screen

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -7,30 +7,24 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Travel App", style: TextStyle(color: Colors.white)),
-        backgroundColor: Colors.blue[700],
+    return GridView(
+      padding: EdgeInsets.all(10),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 7 / 8,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
       ),
-      body: GridView(
-        padding: EdgeInsets.all(10),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 7 / 8,
-          mainAxisSpacing: 10,
-          crossAxisSpacing: 10,
-        ),
-        children:
-            categoriesData
-                .map(
-                  (category) => CategoryItem(
-                    id: category.id,
-                    title: category.title,
-                    imageUrl: category.imageUrl,
-                  ),
-                )
-                .toList(),
-      ),
+      children:
+          categoriesData
+              .map(
+                (category) => CategoryItem(
+                  id: category.id,
+                  title: category.title,
+                  imageUrl: category.imageUrl,
+                ),
+              )
+              .toList(),
     );
   }
 }
