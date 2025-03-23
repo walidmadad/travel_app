a48c37605ee2bfda8a0e48c71c24c416ce700375
walidmadad <walid.madad123@gmail.com>
2025-03-23 21:08:10 +0100

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/data.dart';
 import './screens/filter_screen.dart';
 import './screens/tabs_screen.dart';
 import './screens/categories_trips_screen.dart';
 import './screens/trip_deatils_screen.dart';
+import './models/trip.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, bool> _filters = {
+    "summer": false,
+    "winter": false,
+    "family": false,
+  };
+
+  List<Trip> _availableTrips = tripsData;
+
+  void _changeFilter(Map<String, bool> filterData) {
+    setState(() {
+      _filters = filterData;
+
+      _availableTrips =
+          tripsData.where((trip) {
+            if (_filters['summer'] == true && trip.isInSummer != true) {
+              return false;
+            }
+            if (_filters['winter'] == true && trip.isInWinter != true) {
+              return false;
+            }
+            if (_filters['family'] == true && trip.isForFamilies != true) {
+              return false;
+            }
+            return true;
+          }).toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,9 +72,10 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (ctx) => TabsScreen(),
-        CategoriesTripsScreen.screenRoute: (ctx) => CategoriesTripsScreen(),
+        CategoriesTripsScreen.screenRoute:
+            (ctx) => CategoriesTripsScreen(availableTrips: _availableTrips),
         TripDeatilsScreen.routeName: (ctx) => TripDeatilsScreen(),
-        FilterScreen.screenRoute : (ctx) => FilterScreen(),
+        FilterScreen.screenRoute: (ctx) => FilterScreen(_filters,_changeFilter),
       },
     );
   }
